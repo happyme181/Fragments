3c7b2e9f93bf3ecfec55caeb9b43e9ea87820095
happyme181 <nneka.eriavbe@gmail.com>
2021-07-19 17:51:05 +0100

    commit

--- app/src/main/java/com/nneka/fragments/MainActivity.kt
+++ app/src/main/java/com/nneka/fragments/MainActivity.kt
@@ -2,10 +2,27 @@ package com.nneka.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.nneka.fragments.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navigate(AFragment())
     }
+    fun navigate (fragment: Fragment){
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add (R.id.fragment_container_view, fragment)
+        }
+    }
+
 }
